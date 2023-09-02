49e9000909fb8fe0ef17857b2f4cc646ff20095f
Luis <88600848+LuixDev@users.noreply.github.com>
2023-09-01 21:30:25 -0500

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -81,7 +81,7 @@ const Contact = () => {
           <h1>Tell us how we <br />can help you</h1>
         </div>
 
-        <div style={{ position: 'absolute', top:-50, left: 15, width: '90%', height: '90%', display: 'flex', flexDirection: 'column', alignItems: 'flex-end', justifyContent: 'flex-end', padding: '0px', color: '#9B9B9B', fontSize: '14px' }}>
+        <div style={{ position: 'absolute', top:0, left: 15, width: '90%', height: '90%', display: 'flex', flexDirection: 'column', alignItems: 'flex-end', justifyContent: 'flex-end', padding: '0px', color: '#9B9B9B', fontSize: '14px' }}>
     {/* Contenido del formulario aquí */}
       
         <Card style={{ width: '100%', maxWidth: '32rem', padding: '1rem' }}>
